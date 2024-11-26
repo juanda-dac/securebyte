52e64fba301c1728d27857a36560e891dcb9d7c9
Juan <email@email.com>
2024-11-25 20:50:59 -0500

    Adding features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,32 +9,39 @@ export default function Header({ page = "home" }: Props) {
     const [open, setOpen] = useState(false);
 
     return (
-        <header className="font-montserrat w-full flex flex-col fixed z-10 bg-gray-500/50">
-            <button id="openMenu" className="flex justify-start px-3 py-2 w-full md:hidden" onClick={() => setOpen(!open)}>
-                {
-                    open ? (<ion-icon
+        <header className="font-montserrat w-full flex flex-col fixed z-10 bg-gray-900">
+            <button
+                id="openMenu"
+                className="flex justify-start px-3 py-2 w-full md:hidden"
+                onClick={() => setOpen(!open)}
+            >
+                {open ? (
+                    <ion-icon
                         name="close-circle-outline"
                         class="text-yellow-500"
-                        style={{ 
-                            transform: open ? "rotate(-180deg)" : "rotate(180deg)",
-                            transition: "transform 500ms ease-in-out",    
+                        style={{
+                            transform: open
+                                ? "rotate(-180deg)"
+                                : "rotate(180deg)",
+                            transition: "transform 500ms ease-in-out",
                         }}
                         size="large"
-                    ></ion-icon>):(<ion-icon
+                    ></ion-icon>
+                ) : (
+                    <ion-icon
                         name="menu-outline"
                         class="text-yellow-500"
                         size="large"
-                    ></ion-icon>)
-                }
-                
+                    ></ion-icon>
+                )}
             </button>
 
-            <nav className={`w-full justify-end bg-gray-500/50 absolute top-12 transition-[left] duration-500 ${open ? "left-0":"left-[-100%]"} md:static`} style={{ 
-                
-                
-             }}>
+            <nav
+                className={`w-full justify-end bg-gray-900 absolute top-12 transition-[left] duration-500 ${open ? "left-0" : "left-[-100%]"} md:static`}
+                style={{}}
+            >
                 <ul className="flex flex-col md:flex-row md:justify-end divide-y divide-gray-600 md:divide-none">
-                    <li className="px-4 py-2 text-center">
+                    <li className="px-4 py-2 lg:py-4 lg:px-6 text-center">
                         <a
                             href="#home"
                             className="text-yellow-500"
@@ -43,7 +50,7 @@ export default function Header({ page = "home" }: Props) {
                             Inicio
                         </a>
                     </li>
-                    <li className="px-4 py-2 text-center">
+                    <li className="px-4 py-2 lg:py-4 lg:px-6 text-center">
                         <a
                             href="#about-us"
                             className="text-yellow-500"
@@ -53,7 +60,7 @@ export default function Header({ page = "home" }: Props) {
                         </a>
                     </li>
 
-                    <li className="px-4 py-2 text-center">
+                    <li className="px-4 py-2 lg:py-4 lg:px-6 text-center">
                         <a
                             href="#services"
                             className="text-yellow-500"
@@ -62,8 +69,17 @@ export default function Header({ page = "home" }: Props) {
                             Servicios
                         </a>
                     </li>
+                    <li className="px-4 py-2 lg:py-4 lg:px-6 text-center">
+                        <a
+                            href="#app"
+                            className="text-yellow-500"
+                            onClick={() => setOpen(false)}
+                        >
+                            Nuestra App
+                        </a>
+                    </li>
 
-                    <li className="px-4 py-2 text-center">
+                    <li className="px-4 py-2 lg:py-4 lg:px-6 text-center">
                         <a
                             href="#contact"
                             className="text-yellow-500"
